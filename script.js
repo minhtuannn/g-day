01d04da62b0d385eae71db7ac55ef36f8e34115a
minhtuannn <ngominhtuan311@gmail.com>
2026-02-14 23:55:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media1.tenor.com/m/0VvvoP4v9e8AAAAC/hola.gif",    // 0 normal
+    "https://media.tenor.com/h796J-dd0FUAAAAj/utya-utya-duck.gif",  // 1 confused
+    "https://media.tenor.com/vkXcCpPyFM8AAAAj/utya-telegram-duck.gif",             // 2 pleading
+    "https://media.tenor.com/vkXcCpPyFM8AAAAj/utya-telegram-duck.gif",             // 3 sad
+    "https://media1.tenor.com/m/Ge1T5k9YSIwAAAAC/cxyduck-cxydck.gif",       // 4 sadder
+    "https://media1.tenor.com/m/1rGL_hQANasAAAAC/utya-duck.gif",             // 5 devastated
+    "https://media1.tenor.com/m/iUoKQPeDWK0AAAAd/utya-telegram-duck.gif",               // 6 very devastated
+    "https://media1.tenor.com/m/5tdpj5AWd90AAAAd/utya-duck.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
